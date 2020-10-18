05130f89fcb75e732cdc7b1903b656a25e98751d
alvaro <alvarojaramillogarcia@gmail.com>
2020-10-18 20:01:23 +0200

    added content

---
+++ aws_blockchain/lambda.py
@@ -0,0 +1,28 @@
+import boto3
+import logging
+import json
+import requests
+import os
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+def lambda_handler(event, context):
+    
+    kinesis_client = boto3.client('kinesis', region_name='us-east-2')
+    bitcoin_url = os.environ['BITCOIN_URL']
+    bitcoin_stream = os.environ['BITCOIN_STREAM']
+    
+    r = requests.get('https://blockchain.info/ticker')
+    price = r.json()['EUR']['15m']
+    logger.info(price)
+    
+    payload = {'price':price}
+    put_response = kinesis_client.put_record(
+                   StreamName=bitcoin_stream,
+                   Data=json.dumps(payload),
+                   PartitionKey='price')
+    
+    return {
+        'statusCode': 200
+    }
