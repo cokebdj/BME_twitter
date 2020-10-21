510b6bb11ea6d3bc4bc33e2d9a2ea5cc67a9a356
alvaro <alvarojaramillogarcia@gmail.com>
2020-10-22 01:07:00 +0200

    final functions

--- aws_blockchain/lambda.py
+++ aws_blockchain/lambda.py
@@ -3,10 +3,6 @@ import logging
 import json
 import requests
 import os
-from scipy.stats import linregress
-import time
-
-    
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -16,27 +12,15 @@ def lambda_handler(event, context):
     kinesis_client = boto3.client('kinesis', region_name='us-east-2')
     bitcoin_url = os.environ['BITCOIN_URL']
     bitcoin_stream = os.environ['BITCOIN_STREAM']
-    minutes = int(os.environ['MINS'])
-    times = []
-    prices = []
-    
-    for elem in range(minutes):
-        times.append(elem)
-        r = requests.get('https://blockchain.info/ticker')
-        price = r.json()['EUR']['last']
-        logger.info(elem)
-        logger.info(price)
-        prices.append(price)
-        time.sleep(60)
-        
-    slope, intercept, r_value, p_value, std_err = linregress(times, prices)
-    logger.info(slope)
-    
-    payload = {'slope':slope}
+    r = requests.get(bitcoin_url)
+    price = r.json()['EUR']['last']
+    logger.info(price)
+
+    payload = {'price': price}
     put_response = kinesis_client.put_record(
                    StreamName=bitcoin_stream,
                    Data=json.dumps(payload),
-                   PartitionKey='slope')
+                   PartitionKey='price')
     
     return {
         'statusCode': 200
